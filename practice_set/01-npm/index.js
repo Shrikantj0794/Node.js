ab1f058c1f8d7ec116a62e8fb90a0db2733dddf8
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-31 22:03:41 +0530

    create first server

--- practice_set/01-npm/index.js
+++ practice_set/01-npm/index.js
@@ -1,2 +1,16 @@
-const name = 'shrikant'
-console.log(name)
+// const fs = require('fs');
+// const index = fs.readFileSync('index.html', 'utf-8');
+// const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+// const products = data.products;
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8080, () => {
+  console.log(`Example app listening on port ${port}`)
+})
