8ddb556e5b826ec198fa71b824b7a0e870a7541d
Shrikant Jamale <shrikantj0794@gmail.com>
2024-02-01 22:46:10 +0530

    patch and delete operation used

--- practice_set/01-npm/index.js
+++ practice_set/01-npm/index.js
@@ -60,6 +60,21 @@ server.put('/products/:id', (req, res) => {
   products.splice(productIndex,1,{...req.body, id:id})
   res.json('data updated');
 });
+//patch
+server.patch('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  const product = products[productIndex]
+  products.splice(productIndex,1,{...product,...req.body})
+  res.json('data updated');
+});
+// Delete
+server.delete('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  products.splice(productIndex,1)
+  res.json('Successfully deleted');
+});
 server.get('/demo', (req, res) => {
   // res.sendStatus(404);
   // res.json(products)
