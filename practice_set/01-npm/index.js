d838de6886b5d8ae11e2468e4173a96030ce6c06
Shrikant Jamale <shrikantj0794@gmail.com>
2024-02-01 18:50:48 +0530

    middleware2

--- practice_set/01-npm/index.js
+++ practice_set/01-npm/index.js
@@ -3,14 +3,49 @@
 // const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 // const products = data.products;
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//middleware1
+
+// app.use((req, res, next)=>{
+//   console.log(req.method, req.id, req.hostname);
+//   next()
+// })
+
+
+//middleware2
+const auth = (req, res, next)=>{
+
+  if (req.query.password=='123'){
+    next()
+  }else{
+    res.sendStatus(401);
+  }
+}
+
+// app.use(auth);
+
+
+//API - Endpoint - 
+app.get('/',auth, (req, res) => {
+  // res.send('hello');
+  res.json({type: 'GET'})
+});
+
+app.post('/',auth, (req,res)=>{
+  res.json({type: 'POST'})
+});
+app.put('/',auth, (req,res)=>{
+  res.json({type: 'PUT'})
+});
+app.delete('/', (req,res)=>{
+  res.json({type: 'DELETE'})
+});
+app.patch('/', (req,res)=>{
+  res.json({type: 'PATCH'})
+});
 
 app.listen(8080, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port`)
 })
