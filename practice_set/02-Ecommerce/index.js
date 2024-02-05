30bee2e1a3820e9199277a4931c464188c68921e
Shrikant Jamale <shrikantj0794@gmail.com>
2024-02-05 22:07:27 +0530

    server created using express

--- practice_set/02-Ecommerce/index.js
+++ practice_set/02-Ecommerce/index.js
@@ -1,50 +1,21 @@
 const fs = require('fs');
-const http = require('http')
-
+const express = require('express')
 const index = fs.readFileSync('index.html','utf-8')
 const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
 const products = data.products;
-// creating server using node.js
 
+// creating server using express.js
+const server = express();
 
-
-const server = http.createServer((req,res)=>{
-
-    if(req.url.startsWith('/product')){
-        const id = req.url.split('/')[2]
-        const product = products.find(p=>p.id===(+id))
-        console.log(product)
-        
-        res.setHeader('Content-Type','text/html');
-        let modifiedIndex = index
-            .replace('**title**',product.title)
-            .replace('**price**',product.price)
-            .replace('**rating**',product.rating)
-            .replace('**url**',product.thumbnail)
-        res.end(modifiedIndex);
-        return;
-    }
-
-    
-       
-
-    switch (req.url){
-        case '/':
-            res.setHeader('Content-Type','text/html');
-            res.end(index)
-            break;
-        case '/api':
-            res.setHeader('Content-Type','json');
-            res.end(JSON.stringify(data));
-            break;
-        
-        default: 
-        res.writeHead(404)
-        res.end();
-       
-    }
-    console.log('server started')
-    res.setHeader('Dummy','DummyValue')
+server.get('/', (req,res)=>{
+    res.send('hello');
+    // No need to convert into string
+    // res.json(products);
+    // res.sendStatus(404)
 })
 
-server.listen(8080)
+server.listen(8080, ()=>{
+    console.log('server started')
+});
+
+
